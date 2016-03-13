cbcf767d16472aa61792bedc5fd45bc541571126
Alvaro Ulloa <alvarouc@gmail.com>
2016-03-13 12:07:25 -0600

    fixed scoring bug

--- polyssifier.py
+++ polyssifier.py
@@ -2,6 +2,7 @@
 import sys
 import argparse
 import numpy as np
+import pickle as p
 import multiprocessing
 from multiprocessing import Manager, Pool
 import logging
@@ -58,38 +59,38 @@ def poly(data, label, n_folds=10, scale=True, verbose=True,
         logger.setLevel(logging.ERROR)
     logger.info('Building classifiers ...')
     classifiers = collections.OrderedDict()
-    #    classifiers['Multilayer Perceptron'] = {
-    #        'clf': MLP(verbose=0, patience=500, learning_rate=1,
-    #                   n_hidden=10, n_deep=2, l1_norm=0, drop=0),
-    #        'parameters': {}}
-    #classifiers['Nearest Neighbors'] = {
-    #    'clf': KNeighborsClassifier(3),
-    #    'parameters': {'n_neighbors': [1, 5, 10, 20]}}
+    classifiers['Multilayer Perceptron'] = {
+        'clf': MLP(verbose=0, patience=500, learning_rate=1,
+                   n_hidden=10, n_deep=2, l1_norm=0, drop=0),
+        'parameters': {}}
+    classifiers['Nearest Neighbors'] = {
+        'clf': KNeighborsClassifier(3),
+        'parameters': {'n_neighbors': [1, 5, 10, 20]}}
     #classifiers['SVM'] = {
     #    'clf': SVC(C=1, probability=True, cache_size=10000,
     #               class_weight='balanced'),
     #    'parameters': {'kernel': ['rbf', 'poly'],
     #                   'C': [0.01, 0.1, 1]}}
-    #classifiers['Linear SVM'] = {
-    #    'clf': LinearSVC(dual=False, class_weight='balanced'),
-    #    'parameters': {'C': [0.01, 0.1, 1],
-    #                   'penalty': ['l1', 'l2']}}
-    #classifiers['Decision Tree'] = {
-    #    'clf': DecisionTreeClassifier(max_depth=None,
-    #                                  max_features='auto'),
-    #    'parameters': {}}
-    #classifiers['Random Forest'] = {
-    #    'clf': RandomForestClassifier(max_depth=None,
-    #                                  n_estimators=10,
-    #                                  max_features='auto'),
-    #    'parameters': {'n_estimators': list(range(5, 20))}}
+    classifiers['Linear SVM'] = {
+        'clf': LinearSVC(dual=False, class_weight='balanced'),
+        'parameters': {'C': [0.01, 0.1, 1],
+                       'penalty': ['l1', 'l2']}}
+    classifiers['Decision Tree'] = {
+        'clf': DecisionTreeClassifier(max_depth=None,
+                                      max_features='auto'),
+        'parameters': {}}
+    classifiers['Random Forest'] = {
+        'clf': RandomForestClassifier(max_depth=None,
+                                      n_estimators=10,
+                                      max_features='auto'),
+        'parameters': {'n_estimators': list(range(5, 20))}}
     classifiers['Logistic Regression'] = {
         'clf': LogisticRegression(fit_intercept=True, solver='lbfgs',
                                   penalty='l2'),
         'parameters': {'C': [0.001, 0.1, 1]}}
-    #classifiers['Naive Bayes'] = {
-    #    'clf': GaussianNB(),
-    #    'parameters': {}}
+    classifiers['Naive Bayes'] = {
+        'clf': GaussianNB(),
+        'parameters': {}}
     #classifiers['Voting'] = {}
 
     # Remove classifiers that want to be excluded
@@ -155,24 +156,20 @@ def poly(data, label, n_folds=10, scale=True, verbose=True,
     for clf_name in classifiers:
         temp = np.zeros((n_class, n_class))
         temp_pred = np.zeros((data.shape[0], ))
-        for n, (train_score, test_score, prediction,
-                confusion) in enumerate(result):
-            scores.loc[n_fold, (clf_name, 'train')] = train_score
-            scores.loc[n_fold, (clf_name, 'test')] = test_score
+        for n in range(n_folds):
+            train_score, test_score, prediction, confusion = result.pop(0)
+            scores.loc[n % n_folds, (clf_name, 'train')] = train_score
+            scores.loc[n % n_folds, (clf_name, 'test')] = test_score
             temp += confusion
-            temp_pred[kf[n % n_fold][1]] = _le.inverse_transform(prediction)
+            temp_pred[kf[n % n_folds][1]] = _le.inverse_transform(prediction)
 
         confusions[clf_name] = temp
         predictions[clf_name] = temp_pred
     return scores, confusions, predictions
 
 
-def _scorer(clf, xx, yy):
-    # Scoring
-    #if scoring == 'auc':
-    score = roc_auc_score(xx, yy)
-    #if scoring == 'f1':
-    #    score = f1_score(xx, yy, 'weighted')
+def _scorer(clf, X, y):
+    score = roc_auc_score(y, clf.predict(X))
     return score
 
 
@@ -184,7 +181,6 @@ def fit_clf(args, clf_name, val, n_fold, project_name, save, scoring):
     train, test = args[0]['kf'][n_fold]
     X = args[0]['X'][train, :]
     y = args[0]['y'][train]
-    n_class = len(np.unique(y))
     file_name = '{}_models/{}_{}.p'.format(project_name, clf_name, n_fold+1)
     start = time.time()
     if os.path.isfile(file_name):
@@ -194,20 +190,19 @@ def fit_clf(args, clf_name, val, n_fold, project_name, save, scoring):
         logger.info('Training {} {}'.format(clf_name, n_fold))
         clf = deepcopy(val['clf'])
         if val['parameters']:
-            clf = GridSearchCV(clf, val['parameters'], n_jobs=1, cv=3)
+            clf = GridSearchCV(clf, val['parameters'], n_jobs=1, cv=3,
+                               scoring=_scorer)
         clf.fit(X, y)
         if save:
             joblib.dump(clf, file_name)
 
-    ypred = clf.predict(X)
-
-    train_score = _scorer(y, ypred, scoring, n_class)
+    train_score = _scorer(clf, X, y)
 
     X = args[0]['X'][test, :]
     y = args[0]['y'][test]
-    ypred = clf.predict(X)
     # Scores
-    test_score = _scorer(y, ypred, scoring, n_class)
+    test_score = _scorer(clf, X, y)
+    ypred = clf.predict(X)
     confusion = confusion_matrix(y, ypred)
     duration = time.time()-start
     logger.info('{0:25} {1:2}:  Train {2:.2f}/ Test {3:.2f}, {4:.2f} sec'.format(clf_name, n_fold, train_score, test_score, duration))
@@ -245,13 +240,12 @@ def plot(scores, file_name='temp', min_val=None):
     ax1.legend(loc='upper center', bbox_to_anchor=(0.5, 1.15),
                ncol=2, fancybox=True, shadow=True)
     plt.savefig(file_name + '.pdf')
-    plt.savefig(file_name + '.svg', transparent=False,
-                bbox_inches='tight', pad_inches=0)
+    plt.savefig(file_name + '.svg', transparent=False)
 
     # saving confusion matrices
-    #with open('confusions.pkl', 'wb') as f:
-    #    p.dump(self.confusions, f, protocol=2)
-
+    with open('confusions.pkl', 'wb') as f:
+        p.dump(self.confusions, f, protocol=2)
+    print(scores)
     return (ax1)
 
 def make_argument_parser():
