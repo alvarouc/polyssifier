c3c7c81fb3ca8d6ff3940979b22a7be2132d8d58
Alvaro Ulloa <alvarouc@gmail.com>
2017-03-16 15:27:48 -0400

    dask ignores n_jobs

--- polyssifier/polyssifier.py
+++ polyssifier/polyssifier.py
@@ -198,7 +198,7 @@ def fit_clf(args, clf_name, val, n_fold, project_name, save, scoring):
         clf = deepcopy(val['clf'])
         if val['parameters']:
             clf = GridSearchCV(clf, val['parameters'], cv=3,
-                               scoring=_scorer)
+                               scoring=_scorer, n_jobs=-1)
         clf.fit(X, y)
         if save:
             joblib.dump(clf, file_name)
