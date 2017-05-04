0c27bcdd3a05f0b945a926453e54877582b97099
Alvaro Ulloa <alvarouc@gmail.com>
2017-05-04 12:20:43 -0400

    refactor to separate logging and reporting

---
+++ polyssifier/report.py
@@ -0,0 +1,111 @@
+import numpy as np
+import matplotlib.pyplot as plt
+from utils.logger import make_logger
+
+log = make_logger('Report')
+
+
+class Report(object):
+    """Report class that contains results from runnning polyssifier
+    """
+
+    def __init__(self, scores, confusions, predictions,
+                 test_prob, coefficients):
+        self.scores = scores
+        self.confusions = confusions
+        self.predicitons = predictions
+        self.test_proba = test_prob
+        self.coefficients = coefficients
+
+    def plot(self, path='temp'):
+        plot(self.scores, path)
+
+    def plot_features(self, ntop=10, path='temp',
+                      coef_names=None):
+        plot_features(coefs=self.coefficients,
+                      coef_names=None,
+                      ntop=ntop, file_name=path)
+
+import pdb
+def plot_features(coefs, coef_names=None,
+                  ntop=10, file_name='temp'):
+    fs = {key: np.array(val).squeeze()
+          for key, val in coefs.items()
+          if val[0] is not None}
+
+    n_coefs = fs[list(fs.keys())[0]].shape[1]
+    if coef_names is None:
+        coef_names = np.array([str(c) for c in range(n_coefs)])
+
+    for key, val in fs.items():
+        figure_path = file_name + '_' + key + '_feature_ranking.png'
+        log.info('Plotting %s coefs to %s', key, figure_path)
+        plt.figure(figsize=(10, 10))
+        # plotting coefficients weights
+        mean = np.mean(val, axis=0)
+        std = np.std(val, axis=0)
+        idx = np.argsort(np.abs(mean))
+        topm = mean[idx[-ntop:]][::-1]
+        tops = std[idx[-ntop:]][::-1]
+        plt.subplot(211)
+        plt.bar(range(ntop), topm, yerr=tops,
+                tick_label=coef_names[idx[-ntop:]])
+        plt.title('{}: Feature importance'.format(key))
+        plt.xlabel('Feature index')
+
+        # plotting coefficients rank
+        rank = np.apply_along_axis(np.argsort, axis=1,
+                                   arr=np.abs(val)) + 1
+        rank_mean = rank.mean(axis=0)
+        rank_std = rank.std(axis=0)
+        idx = np.argsort(rank_mean)
+        topm = rank_mean[idx[:ntop]]
+        tops = rank_std[idx[:ntop]]
+
+        plt.subplot(212)
+        plt.bar(range(ntop), topm, yerr=tops,
+                tick_label=coef_names[idx[-ntop:]])
+        plt.title('{}: Feature rank'.format(key))
+        plt.xlabel('Feature index')
+        plt.grid(axis='y')
+        plt.tight_layout()
+        plt.savefig(figure_path)
+
+
+
+
+def plot(scores, file_name='temp', min_val=None):
+
+    df = scores.apply(np.mean).unstack().join(
+        scores.apply(np.std).unstack(), lsuffix='_mean', rsuffix='_std')
+    df.columns = ['Test score', 'Train score', 'Test std', 'Train std']
+    df.sort_values('Test score', ascending=False, inplace=True)
+    error = df[['Train std', 'Test std']]
+    error.columns = ['Train score', 'Test score']
+    data = df[['Train score', 'Test score']]
+
+    nc = df.shape[0]
+
+    ax1 = data.plot(kind='bar', yerr=error, colormap='coolwarm',
+                    figsize=(nc * 2, 5), alpha=1)
+    #ax1.set_axis_bgcolor((.7, .7, .7))
+    ax1.legend(loc='lower center', bbox_to_anchor=(0.5, 1.05),
+               ncol=2, fancybox=True, shadow=True)
+
+    ax1.set_xticklabels([])
+    ax1.set_xlabel('')
+    ax1.yaxis.grid(True)
+
+    temp = np.array(data)
+    ylim = np.max(temp.min() - .1, 0) if min_val is None else min_val
+    ax1.set_ylim(ylim, 1)
+    for n, rect in enumerate(ax1.patches):
+        if n >= nc:
+            break
+        ax1.text(rect.get_x() - rect.get_width() / 2., ylim + (1 - ylim) * .01,
+                 data.index[n], ha='center', va='bottom',
+                 rotation='90', color='black', fontsize=15)
+    plt.tight_layout()
+    plt.savefig(file_name + '.pdf')
+    plt.savefig(file_name + '.svg', transparent=False)
+    return (ax1)
---
+++ utils/logger.py
@@ -0,0 +1,19 @@
+import logging
+
+
+def make_logger(name=''):
+    formatter = logging.Formatter(
+        '%(asctime)s - %(name)s - %(levelname)s - %(message)s')
+    logger = logging.getLogger(name)
+    logger.setLevel(logging.INFO)
+    fh = logging.FileHandler('{}.log'.format(name))
+    fh.setLevel(logging.DEBUG)
+    fh.setFormatter(formatter)
+    ch = logging.StreamHandler()
+    ch.setLevel(logging.INFO)
+    ch.setFormatter(formatter)
+    # add the handlers to the logger
+    logger.addHandler(fh)
+    logger.addHandler(ch)
+
+    return logger
