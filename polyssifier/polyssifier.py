217f69a0db15988bf12cbbc180dc2ca7fb56660e
seyerly <seyerly@seas.upenn.edu>
2017-06-19 10:37:12 -0400

    Updated stratified K_Fold.

--- polyssifier/polyssifier.py
+++ polyssifier/polyssifier.py
@@ -88,10 +88,9 @@ def poly(data, label, n_folds=10, scale=True, exclude=[],
     logger.info('Initialization, done.')
 
     # This provides train/test indices to split data in train/test sets.
-    skf = KFold(n_splits=n_folds, random_state=1988)
+    skf = StratifiedKFold(n_splits=n_folds, random_state=1988)
     skf.get_n_splits(np.zeros(data.shape[0]), label)
     kf = list(skf.split(np.zeros(data.shape[0]), label))
-
     # Parallel processing of tasks
     manager = Manager()
     args = manager.list()
