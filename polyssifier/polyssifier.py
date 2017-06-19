3e98cc58f875cde853aaeff98f95df5d9d371f3a
seyerly <seyerly@seas.upenn.edu>
2017-06-19 15:36:59 -0400

    Fixed typo

--- polyssifier/polyssifier.py
+++ polyssifier/polyssifier.py
@@ -264,7 +264,7 @@ if __name__ == '__main__':
     logger.info(
         'Starting classification with {} workers'.format(args.concurrency))
 
-    report = poly(data, label, n_folds=5, project_name=args.name,
+    report = polyr(data, label, n_folds=5, project_name=args.name,
                   concurrency=int(args.concurrency))
     report.plot_scores(os.path.join('poly_' + args.name, args.name))
     report.plot_features(os.path.join('poly_' + args.name, args.name))
