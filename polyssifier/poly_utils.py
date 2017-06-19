ebbf832eeab89c68745c0cbaa3bfe83dc472205e
seyerly <seyerly@seas.upenn.edu>
2017-06-19 11:11:05 -0400

    .

--- polyssifier/poly_utils.py
+++ polyssifier/poly_utils.py
@@ -163,7 +163,7 @@ def build_regressors(exclude, scale, feature_selection, nCols):
     if 'GaussianProcessRegressor' not in exclude:
         regressors['GaussianProcessRegressor'] = {
             'reg': GaussianProcessRegressor(),
-            'parameters': {'alpha': [0.0000000001, 0.00000000015, 0.000000001]}
+            'parameters': {}
         }
 
     if 'Ridge' not in exclude:
