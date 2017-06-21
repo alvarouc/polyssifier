8f3f9a74fcc05448466bd6f85ec7e83bc345ed09
seyerly <seyerly@seas.upenn.edu>
2017-06-21 16:08:49 -0400

    Made a separate file for regression tests.

---
+++ tests/regression_tests.py
@@ -0,0 +1,36 @@
+import pytest
+import warnings
+
+from polyssifier import polyr
+from sklearn.datasets import make_classification, load_diabetes
+
+warnings.filterwarnings("ignore", category=DeprecationWarning)
+diabetes_data = load_diabetes().data
+diabetes_target = load_diabetes().target
+
+@pytest.mark.medium
+def test_run_regression():
+    report = polyr(diabetes_data, diabetes_target, n_folds=10, verbose=1,
+                  feature_selection=False, scoring='r2',
+                  save=False, project_name='test3')
+    assert (report.scores.mean()[:, 'test'] > 0.2).all(),\
+        'test score below chance'
+    assert (report.scores.mean()[:, 'train'] > 0.2).all(),\
+        'train score below chance'
+
+@pytest.mark.medium
+def test_feature_selection_regression():
+    report = polyr(diabetes_data, diabetes_target, n_folds=10, verbose=1,
+                  feature_selection=True, scoring='r2',
+                  save=False, project_name='test3')
+    assert (report.scores.mean()[:, 'test'] > 0.2).all(),\
+        'test score below chance'
+    assert (report.scores.mean()[:, 'train'] > 0.2).all(),\
+        'train score below chance'
+
+@pytest.mark.medium
+def test_plot_regression():
+    report = polyr(diabetes_data, diabetes_target, n_folds=10, verbose=1,
+                  feature_selection=False, scoring='r2',
+                  save=False, project_name='test3')
+    report.plot_scores()
