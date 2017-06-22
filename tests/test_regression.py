35ebfd1ab5bcc316306caa5e0f266889727d1b91
seyerly <seyerly@seas.upenn.edu>
2017-06-22 11:52:16 -0400

    Removed redundant calls to polyr in order to speed up testing.

--- tests/test_regression.py
+++ tests/test_regression.py
@@ -8,6 +8,10 @@ warnings.filterwarnings("ignore", category=DeprecationWarning)
 diabetes_data = load_diabetes().data
 diabetes_target = load_diabetes().target
 
+report_with_features = polyr(diabetes_data, diabetes_target, n_folds=2,
+                   verbose=1, concurrency=1,
+                   feature_selection=True, scoring='r2',
+                   save=False, project_name='test3')
 
 @pytest.mark.medium
 def test_run_regression():
@@ -20,23 +24,14 @@ def test_run_regression():
     assert (report.scores.mean()[:, 'train'] > 0.2).all(),\
         'train score below chance'
 
-
 @pytest.mark.medium
 def test_feature_selection_regression():
-    report = polyr(diabetes_data, diabetes_target, n_folds=2,
-                   verbose=1, concurrency=1,
-                   feature_selection=True, scoring='r2',
-                   save=False, project_name='test3')
-    assert (report.scores.mean()[:, 'test'] > 0.2).all(),\
+    assert (report_with_features.scores.mean()[:, 'test'] > 0.2).all(),\
         'test score below chance'
-    assert (report.scores.mean()[:, 'train'] > 0.2).all(),\
+    assert (report_with_features.scores.mean()[:, 'train'] > 0.2).all(),\
         'train score below chance'
-
 
 @pytest.mark.medium
 def test_plot_regression():
-    report = polyr(diabetes_data, diabetes_target, n_folds=2, verbose=1,
-                   feature_selection=False, scoring='r2',
-                   save=False, project_name='test3')
-    report.plot_scores()
-    report.plot_features()
+    report_with_features.plot_scores()
+    report_with_features.plot_features()
