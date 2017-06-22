cdb343ce1ac16fb28e3f1d18566f5e176a33eb15
Alvaro Ulloa <alvarouc@gmail.com>
2017-06-22 15:40:51 -0400

    isolating tests

--- tests/test_regression.py
+++ tests/test_regression.py
@@ -8,13 +8,23 @@ warnings.filterwarnings("ignore", category=DeprecationWarning)
 diabetes_data = load_diabetes().data
 diabetes_target = load_diabetes().target
 
-report_with_features = polyr(diabetes_data, diabetes_target, n_folds=2,
-                   verbose=1, concurrency=1,
-                   feature_selection=True, scoring='r2',
-                   save=False, project_name='test3')
+
+@pytest.mark.medium
+def test_feature_selection_regression():
+    global report_with_features
+    report_with_features = polyr(diabetes_data, diabetes_target, n_folds=2,
+                                 verbose=1, concurrency=1,
+                                 feature_selection=True, scoring='r2',
+                                 save=False, project_name='test3')
+    assert (report_with_features.scores.mean()[:, 'test'] > 0.2).all(),\
+        'test score below chance'
+    assert (report_with_features.scores.mean()[:, 'train'] > 0.2).all(),\
+        'train score below chance'
+
 
 @pytest.mark.medium
 def test_run_regression():
+    global report
     report = polyr(diabetes_data, diabetes_target, n_folds=2,
                    verbose=1, concurrency=1,
                    feature_selection=False, scoring='r2',
@@ -24,14 +34,14 @@ def test_run_regression():
     assert (report.scores.mean()[:, 'train'] > 0.2).all(),\
         'train score below chance'
 
-@pytest.mark.medium
-def test_feature_selection_regression():
-    assert (report_with_features.scores.mean()[:, 'test'] > 0.2).all(),\
-        'test score below chance'
-    assert (report_with_features.scores.mean()[:, 'train'] > 0.2).all(),\
-        'train score below chance'
 
 @pytest.mark.medium
-def test_plot_regression():
+def test_plot_scores_no_selection():
+    report.plot_scores()
+    report.plot_features()
+
+
+@pytest.mark.medium
+def test_plot_features_with_selection():
     report_with_features.plot_scores()
     report_with_features.plot_features()
