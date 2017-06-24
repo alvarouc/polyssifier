e0041ad20453fd39f4a4c54c9f0bac1b97e59279
Alvaro Ulloa <alvarouc@gmail.com>
2017-06-23 22:46:14 -0400

    fixes compatibility with python 2.7

--- tests/test_regression.py
+++ tests/test_regression.py
@@ -21,7 +21,7 @@ def test_feature_selection_regression():
     assert (report_with_features.scores.mean()[:, 'train'] > 0.2).all(),\
         'train score below chance'
 
-    for key, ypred in report_with_features.predictions.items():
+    for key, ypred in report_with_features.predictions.iteritems():
         mse = np.linalg.norm(ypred - diabetes_target) / len(diabetes_target)
         assert mse < 5, '{} Prediction error is too high'.format(key)
 
