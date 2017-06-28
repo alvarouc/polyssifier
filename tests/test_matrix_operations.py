3339723b561f73c3aac8ac8f57089a6c5bf0c032
seyerly <seyerly@seas.upenn.edu>
2017-06-28 10:09:16 -0400

    Changed assert statements.

--- tests/test_matrix_operations.py
+++ tests/test_matrix_operations.py
@@ -8,10 +8,10 @@ def test_exponent_matrix():
     matrix_2pow = np.matrix('1 4; 9 16')
     matrix_3pow = np.matrix('1 8; 27 64')
     matrix_4pow = np.matrix('1 16; 81 256')
-    assert all(np.array_equal(matrix, exponent_matrix(matrix, 1)))
-    assert all(np.array_equal(matrix_2pow, exponent_matrix(matrix, 2)))
-    assert all(np.array_equal(matrix_3pow, exponent_matrix(matrix, 3)))
-    assert all(np.array_equal(matrix_4pow, exponent_matrix(matrix, 4)))
+    assert (matrix == exponent_matrix(matrix, 1)).all()
+    assert (matrix_2pow == exponent_matrix(matrix, 2)).all()
+    assert (matrix_3pow == exponent_matrix(matrix, 3)).all()
+    assert (matrix_4pow == exponent_matrix(matrix, 4)).all()
 
 @pytest.mark.medium
 def test_create_multivariate():
@@ -19,6 +19,6 @@ def test_create_multivariate():
     poly1 = np.matrix('5 6; 7 8')
     poly2 = np.matrix('5 6 25 36; 7 8 49 64')
     poly3 = np.matrix('5 6 25 36 125 216; 7 8 49 64 343 512')
-    assert all(np.array_equal(poly1, create_multivariate(matrix, 1)))
-    assert all(np.array_equal(poly2, create_multivariate(matrix, 2)))
-    assert all(np.array_equal(poly3, create_multivariate(matrix, 3)))
+    assert (poly1 == create_multivariate(matrix, 1)).all()
+    assert (poly2 == create_multivariate(matrix, 2)).all()
+    assert (poly3 == create_multivariate(matrix, 3)).all()
