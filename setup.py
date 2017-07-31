3d78a2e76cb4ff4a3f09edb253e1d05c9737ecca
Alvaro Ulloa <alvarouc@gmail.com>
2017-07-31 17:01:21 -0400

    improved setup

--- setup.py
+++ setup.py
@@ -1,17 +1,80 @@
-from setuptools import setup
+"""A setuptools based setup module.
+
+See:
+https://packaging.python.org/en/latest/distributing.html
+https://github.com/pypa/sampleproject
+"""
+
+# Always prefer setuptools over distutils
+from setuptools import setup, find_packages
+# To use a consistent encoding
+from codecs import open
+from os import path
+
+here = path.abspath(path.dirname(__file__))
+
+# Get the long description from the README file
+with open(path.join(here, 'README.md'), encoding='utf-8') as f:
+    long_description = f.read()
+
 setup(
     name='polyssifier',
-    packages=['polyssifier'],
-    version='0.5.1',
-    install_requires=[
-        'pandas',
-        'sklearn',
-        'numpy'],
+
+    # Versions should comply with PEP440.  For a discussion on single-sourcing
+    # the version across setup.py and the project code, see
+    # https://packaging.python.org/en/latest/single_source_version.html
+    version='0.5.2',
+
     description='Data exploration tool for assessing optimal classification methods',
+    long_description=long_description,
+
+    # The project's main homepage.
+    url='https://github.com/alvarouc/polyssifier',
+
+    # Author details
     author='Alvaro Ulloa',
     author_email='alvarouc@gmail.com',
-    url='https://github.com/alvarouc/polyssifier',
-    download_url='https://github.com/alvarouc/polyssifier/tarball/0.5.1',
-    keywords=['classification', 'machine learning', 'data science'],
-    classifiers=[],
+
+    # Choose your license
+    license='GPLv3',
+
+    # See https://pypi.python.org/pypi?%3Aaction=list_classifiers
+    classifiers=[
+        # How mature is this project? Common values are
+        #   3 - Alpha
+        #   4 - Beta
+        #   5 - Production/Stable
+        'Development Status :: 3 - Alpha',
+
+        # Indicate who your project is intended for
+        'Intended Audience :: Developers',
+        'Topic :: Software Development :: Build Tools',
+
+        # Pick your license as you wish (should match "license" above)
+        'License :: OSI Approved :: GNU General Public License v3 (GPLv3)'
+
+        # Specify the Python versions you support here. In particular, ensure
+        # that you indicate whether you support Python 2, Python 3 or both.
+        'Programming Language :: Python :: 2',
+        'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.3',
+        'Programming Language :: Python :: 3.4',
+        'Programming Language :: Python :: 3.5',
+        'Programming Language :: Python :: 3.6',
+    ],
+
+    # What does your project relate to?
+    keywords='classification machine learning data science',
+
+    # You can just specify the packages manually here if your project is
+    # simple. Or you can use find_packages().
+    packages=['polyssifier', ],
+
+    # List run-time dependencies here.  These will be installed by pip when
+    # your project is installed. For an analysis of "install_requires" vs pip's
+    # requirements files see:
+    # https://packaging.python.org/en/latest/requirements.html
+    install_requires=['pandas', 'sklearn', 'numpy', 'matplotlib'],
+
 )
