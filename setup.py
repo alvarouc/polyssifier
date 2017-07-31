902ac7ebf45f1462fc9c697e27aac44a9d51121d
Alvaro Ulloa <alvarouc@gmail.com>
2017-07-31 17:07:12 -0400

    0.55

--- setup.py
+++ setup.py
@@ -13,20 +13,15 @@ from os import path
 
 here = path.abspath(path.dirname(__file__))
 
-# Get the long description from the README file
-with open(path.join(here, 'README.md'), encoding='utf-8') as f:
-    long_description = f.read()
-
 setup(
     name='polyssifier',
 
     # Versions should comply with PEP440.  For a discussion on single-sourcing
     # the version across setup.py and the project code, see
     # https://packaging.python.org/en/latest/single_source_version.html
-    version='0.5.4',
+    version='0.5.5',
 
     description='Data exploration tool for assessing optimal classification methods',
-    long_description=long_description,
 
     # The project's main homepage.
     url='https://github.com/alvarouc/polyssifier',
